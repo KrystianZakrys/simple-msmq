fd6758cbe66c7336703e3ce199c0361dfeae648e
Krystian Zakryś <krystian.zakrys@gmail.com>
2018-06-10 21:07:57 +0200

    Easy and simple MSMQ apps [ SENDER amd RECEIVER ]

--- MSMQ/Program.cs
+++ MSMQ/Program.cs
@@ -3,13 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Messaging;
 
-namespace MSMQ
+namespace Sender
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter message to be sent");
+            Console.WriteLine("(High priority messages should start with '!:')");
+            string message = Console.ReadLine();
+
+
+            MessageQueue queue;
+            if (MessageQueue.Exists(@".\Private$\queue"))
+            {
+                queue = new MessageQueue(@".\Private$\queue");
+            }
+            else
+            {
+                queue = MessageQueue.Create(@".\Private$\queue");
+            }
+
+            Message myMessage = new Message();
+
+            myMessage.Formatter = new BinaryMessageFormatter();
+            myMessage.Body = message;
+            myMessage.Label = "SenderMessage";
+
+            if(message.Contains("!:"))
+            {
+                myMessage.Priority = MessagePriority.Highest;
+            }
+            else
+            {
+                myMessage.Priority = MessagePriority.Normal;
+            }
+
+            queue.Send(myMessage);
+            Console.WriteLine("Your message has been sent! Good luck repairing your OS!");
+            Console.ReadKey();
         }
     }
 }
---
+++ Receiver/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Newest message: ");
+
+            MessageQueue queue;
+            queue = new MessageQueue(@".\Private$\queue");
+
+            Message message = queue.Receive();
+            message.Formatter = new BinaryMessageFormatter();
+
+            Console.WriteLine(message.Body.ToString());
+            Console.ReadKey();
+            
+        }
+    }
+}
